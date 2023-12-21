97630dbaef4897fcd40da1baa0f6fc4e88288f09
killer-goose <152170803+killer-goose@users.noreply.github.com>
2023-12-21 14:02:17 +0530

    desktop site works

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,28 @@ export default function App() {
   return (
     // Main is the 'container' itself, therefore no separate component for adjusting margins is introduced
 
-    <main className='main-bg min-h-screen bg-primary-red px-6 py-5 text-center text-white'>
-      <section className='main-text'>
-        <h1 className='mx-auto mb-6 mt-10 max-w-64 text-3xl font-bold'>
-          Learn to code by watching others
-        </h1>
-        <p className='mx-auto max-w-xs font-semibold'>
-          See how experienced developers solve problems in real-time. Watching
-          scripted tutorials is great, but understanding how developers think is
-          invaluable.
-        </p>
-      </section>
-      <section className='form-component'>
-        <div className='pricing shadow-custom mb-6 mt-12 rounded-xl bg-accent-blue py-4'>
-          <div className='text-container mx-auto max-w-52 tracking-wide'>
-            <b className='font-bold'>Try it free 7 days</b> then $20/mo.
-            thereafter
+    <main className='main-bg min-h-screen bg-primary-red px-6 pb-10 pt-5 text-center text-white lg:px-12'>
+      <div className='content-container items-center gap-8 lg:flex max-w-screen-xl mx-auto xl:gap-0 xl:justify-around'>
+        <section className='main-text lg:text-left'>
+          <h1 className='xs:max-w-lg mx-auto mb-6 mt-10 max-w-64 text-3xl font-bold sm:text-4xl lg:text-5xl'>
+            Learn to code by watching others
+          </h1>
+          <p className='xs:max-w-md mx-auto max-w-xs lg:mx-[unset] font-semibold text-[--white-200]'>
+            See how experienced developers solve problems in real-time. Watching
+            scripted tutorials is great, but understanding how developers think
+            is invaluable.
+          </p>
+        </section>
+        <section className='form-component mx-auto w-full max-w-xl lg:max-w-xl xl:mx-[unset]'>
+          <div className='pricing shadow-custom mb-6 mt-12 rounded-xl bg-accent-blue p-4'>
+            <div className='text-container xs:max-w-none mx-auto max-w-52 tracking-wide'>
+              <b className='font-bold'>Try it free 7 days</b> then $20/mo.
+              thereafter
+            </div>
           </div>
-        </div>
-        <Form />
-      </section>
+          <Form />
+        </section>
+      </div>
     </main>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      screens:{
+        xs:'470px',
+      },
       colors: {
         primary: {
           red: 'hsl(0, 100%, 74%)',
